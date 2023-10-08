82b46d4f87ee73ca70130539d58179d2243524a8
Fl0h0 <neururer.florian@gmail.com>
2023-10-08 18:33:25 +0200

    task 2

---
+++ proseminar/01/job3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Execute job in the partition "lva" unless you have special requirements.
+
+#SBATCH --partition=lva
+
+# Name your job to be able to identify it later
+
+#SBATCH --job-name test
+
+# Redirect output stream to this file
+
+#SBATCH --output=output.log
+
+# Maximum number of tasks (=processes) to start in total
+
+#SBATCH --ntasks=2
+
+# Enforce exclusive node allocation, do not share with other jobs
+
+#SBATCH --exclusive
+
+module load hwloc
+lstopo
